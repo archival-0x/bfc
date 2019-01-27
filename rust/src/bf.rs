d5fc9e1ca79ed443c6b15982c4a61c8b67d32387
ex0dus-0x <ex0dus@codemuch.tech>
2019-01-26 20:55:43 -0500

    Add rust interpreter

---
+++ rust/src/bf.rs
@@ -0,0 +1,88 @@
+//! bf.rs
+//!
+//!     normal Brainfuck program interpreter, as the API can
+//!     work as standalone BF implementation w/out dynamic
+//!     compilation.
+
+
+// defines the default number of cells within
+// a Brainfuck tape.
+const TAPE_SIZE: usize = 30000;
+
+
+/// defines valid instructions for Brainfuck execution
+pub enum BFInstr {
+    IncrementPtr,
+    DecrementPtr,
+    IncrementVal,
+    DecrementVal,
+    Write,
+    Read,
+    BeginLoop,
+    ExitLoop
+}
+
+
+pub struct BFInterpreter {
+    pub inst_buf: Vec<BFInstr>,
+    tape_size: usize,
+}
+
+
+/// defines errors encountered during BFInterpreter interaction
+pub enum BFError {
+    InvalidInstruction,
+    BadTapeSize,
+}
+
+
+impl BFInterpreter {
+
+    /// internal parser helper that converts raw instruction chars
+    /// into valid Brainfuck instructions.
+    fn parse_raw_instr(instruction: char) -> Result<BFInstr, BFError> {
+        match instruction {
+            '>' => Ok(BFInstr::IncrementPtr),
+            '<' => Ok(BFInstr::DecrementPtr),
+            '+' => Ok(BFInstr::IncrementVal),
+            '-' => Ok(BFInstr::DecrementVal),
+            '.' => Ok(BFInstr::Write),
+            ',' => Ok(BFInstr::Read),
+            '[' => Ok(BFInstr::BeginLoop),
+            ']' => Ok(BFInstr::ExitLoop),
+            _   => Err(BFError::InvalidInstruction)
+        }
+    }
+
+
+    /// `new_program` generates a new Brainfuck struct for interaction by
+    /// parsing a raw text of the program.
+    pub fn new_program(&self, program: &str) -> Result<BFInterpreter, BFError> {
+
+        // use filter_map in order to create instr vector iteratively with
+        // instruction parser helper
+        let mut inst_buf: Vec<BFInstr> = program.chars()
+            .filter_map(|c| self.parse_raw_intr(c).is_ok())
+            .collect::<Vec<BFInstr>>();
+
+        Ok(BFInterpreter {
+            inst_buf: inst_buf,
+            tape_size: TAPE_SIZE
+        })
+    }
+
+
+    /// `override_tape` changes the size of Brainfuck tape.
+    /// (default is 30,000).
+    pub fn override_tape(&mut self, size: usize) -> () {
+        self.tape_size = size;
+    }
+
+
+    /// executes the program that was loaded and parsed.
+    pub fn normal_exec(&self) -> Result<(), BFError> {
+        
+        
+        Ok(())
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,13 +1,26 @@
-#[cfg(target_arch="x86_64")]
-#![feature(plugin)]
-#![plugin(dynasm)]
-
 #[macro_use]
 extern crate dynasmrt;
 
+use std::env;
+use std::mem;
+
 use dynasmrt::{DynasmApi, DynasmLabelApi};
+
+mod bf;
+use bf::BFInterpreter;
+
+fn execute_jit(prog: BFInterpreter) -> () {
+    let mut ops = dynasmrt::x64::Assembler::new().unwrap();
+}
 
 
 fn main() {
-    let mut ops = dynasmrt::x64::Assembler::new().unwrap();
+    
+    // parse arguments
+    let mut args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Expected one argument (.bf file), got {}", args.len());
+    }
+   
+    // read file
 }
