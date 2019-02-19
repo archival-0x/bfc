3d2de7e94067e775a251901d5928e1586a8557c9
ex0dus-0x <ex0dus@codemuch.tech>
2019-02-18 20:15:55 -0500

    Improved golang example

--- golang/main.go
+++ golang/main.go
@@ -4,10 +4,27 @@ import (
     "fmt"
     "io/ioutil"
     "os"
+
+    "github.com/antlr/antlr4/runtime/Go/antlr"
+    "./parser"
 )
+
+type BFListener struct {
+    *parser.BasebrainfuckListener
+    tape []int
+}
+
+
+type BFListener interface {
+    antlr ParseTreeListener
+
+    // TODO interface logic
+}
 
 
 func main() {
+
+    // read file from arg
     fileName := os.Args[1]
     code, err := ioutil.ReadFile(fileName)
     if err != nil {
@@ -15,5 +32,13 @@ func main() {
         os.Exit(-1)
     }
 
-    // TODO: run machine
+    // initialize input with ANTLR
+    in_stream = antlr.NewInputStream(code)
+
+    // create lexer
+    lexer := parser.NewbrainfuckLexer(in_stream)
+    stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+    // create parser
+    parser := parser.NewbrainfuckParser(stream)
 }
